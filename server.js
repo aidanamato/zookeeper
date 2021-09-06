c86dca6905378891320fb8d5cd19947056ac6d7f
Aidan Amato <aidanamato@comcast.net>
2021-09-05 22:22:32 -0400

    add get and post routes for frontend

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({extend: true}));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArr) {
   let personalityTraitsArr = [];
@@ -110,6 +111,22 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.listen(PORT, () => {
