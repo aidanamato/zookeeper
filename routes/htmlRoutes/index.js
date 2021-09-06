056536b800388e6f41796f55007657bd4671bd47
Aidan Amato <aidanamato@comcast.net>
2021-09-06 15:04:04 -0400

    add search from in zookeeper.html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,20 @@
+const path = require('path');
 const router = require('express').Router();
-const htmlRouter = require('../htmlRoutes/htmlRoutes');
 
-router.use(htmlRouter);
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'))
+});
+
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'))
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 module.exports = router;
